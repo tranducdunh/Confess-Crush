67be148cbcd588c2e3439536285041cb80730460
tranducdunh <96191692+tranducdunh@users.noreply.github.com>
2021-12-15 23:05:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Demo",
+    titleWeb: "thế giới cảu anh",
     introTitle: 'cậu phải trả lời thật lòng',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'cậu yêu tớ phải không🥰',
+    desc: 'cậu không trả lời hoặc thoát ra,tức là cậu vợ tớ đó nha:> ',
     btnYes: 'Thích lắm <33',
     btnNo: 'cậu mơ à :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'vậy thui tớ thương cậu hết mực vây.',
+    reply: 'tại vì cậu đẹp trai hihi<33333333',
+    mess: 'Mình biết mà 🥰. love you 300.000😘😘',
+    messDesc: 'vậy thì inbox với mình nào còn chờ gì nx cậu iu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100015663631965' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
